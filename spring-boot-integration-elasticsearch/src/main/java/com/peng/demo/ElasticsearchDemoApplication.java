6f0930f2b301f3044e7073e408f1daf6a2ed7d3f
PengG <2225625042@qq.com>
2020-09-28 15:05:50 +0800

    初始化倉庫

---
+++ spring-boot-integration-elasticsearch/src/main/java/com/peng/demo/ElasticsearchDemoApplication.java
@@ -0,0 +1,16 @@
+package com.peng.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author PengG
+ * @date 2020/9/25 16:54
+ */
+@SpringBootApplication
+public class ElasticsearchDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchDemoApplication.class, args);
+    }
+}
---
+++ spring-boot-integration-elasticsearch/src/main/java/com/peng/demo/config/ElasticsearchConfig.java
@@ -0,0 +1,12 @@
+package com.peng.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author PengG
+ * @date 2020/9/25 19:05
+ */
+@Configuration
+public class ElasticsearchConfig {
+
+}
